1bf85bfd79b0eb19a28b8b355a920414a75d01da
genbien <genbien@gmail.com>
2017-01-14 23:28:03 +0100

    fix: mess

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.get('/', function (req, res) {
   res.send('<html><body><img src="https://shechive.files.wordpress.com/2012/02/a-kitty-cat-7.jpg"/></body></html>')
 })
 
-app.listen(3000, function () {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!')
 })
-
-console.log()
