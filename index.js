9edc1de7369ff6c1cdc55b9ea5e4d0859991c1f6
genbien <genbien@gmail.com>
2017-01-16 11:54:43 +0100

    add: tweet feed

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('<html><body><img src="https://shechive.files.wordpress.com/2012/02/a-kitty-cat-7.jpg"/></body></html>')
+  res.send('<html><body><img src="https://shechive.files.wordpress.com/2012/02/a-kitty-cat-7.jpg"/> <a class="twitter-timeline" href="https://twitter.com/mairie5paris?lang=fr">A Twitter List by TwitterDev</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script> </body></html>');
+})
+
+
+app.get('/secret.jpg', function (req, res) {
+  res.send('hello');
 })
 
 const PORT = process.env.PORT || 3000;
