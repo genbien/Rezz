4f9117ef4bc9b02b0586904768af65f2d6fe18cd
genbien <genbien@gmail.com>
2017-01-17 21:47:11 +0100

    upd: fiddling

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express')
+var redis  = require("redis"),
+    client = redis.createClient();
 
-const app = express()
-
-app.get('/', function (req, res) {
-	res.sendFile('index.html', {root: __dirname })
+client.on('error', function (err) {
+  console.log('Error ' + err)
 })
 
-app.get('/secret.jpg', function (req, res) {
-	res.send('hello');
-})
 
-const PORT = process.env.PORT || 3000;
+// const app = express()
 
-app.listen(PORT, function () {
-	console.log('Example app listening on port 3000!')
-})
+// app.get('/', function (req, res) {
+// 	res.sendFile('index.html', {root: __dirname })
+// })
+
+// app.get('/secret.jpg', function (req, res) {
+// 	res.send('hello');
+// })
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, function () {
+// 	console.log('Example app listening on port 3000!')
+// })
+
+client.set("Genevieve", "I'm having a party tonight, if you think there's too much noise, well too bad :P", redis.print);
+
+client.get('Genevieve', function(err, reply) {
+    console.log(reply);
+});
