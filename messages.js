f117708f006763f1f5b2e3484a92e0e3d9574f43
genbien <genbien@gmail.com>
2017-01-24 20:33:00 +0100

    mod: working on date

--- messages.js
+++ messages.js
@@ -9,55 +9,57 @@ const redis = require("redis");
 const redisClient = redis.createClient();
 
 function get_messages() {
-  return q(redisClient, 'keys', 'msg:*')
-    .then(function(keys) {
-      const ids = keys.map(function(key) {
-        return parseInt(key.split(':')[1]);
-      }).sort();
-      return Promise.all(ids.map(function (id) {
-        return q(redisClient, 'get', 'msg:'+id);
-      }))
-    })
-    .then(function(values) {
-      return values.map(function (value) {
-        const matches = /^([^:]*):(.*)$/.exec(value);
-        const [_, username, message] = matches;
-        return { username, message };
-      })
-    });
+	return q(redisClient, 'keys', 'msg:*')
+		.then(function(keys) {
+			const ids = keys.map(function(key) {
+				return parseInt(key.split(':')[1]);
+			}).sort();
+			return Promise.all(ids.map(function (id) {
+				return q(redisClient, 'get', 'msg:'+id);
+			}))
+		})
+		.then(function(values) {
+			return values.map(function (value) {
+				const matches = /^([^:]*):(.*)$/.exec(value);
+				const [_, username, message] = matches;
+				return { username, message };
+			})
+		});
 }
 
 function add_message(username, message) {
-  return q(redisClient, 'incr', 'key:msg')
-    .then(function(key) {
-      return q(redisClient, 'set', 'msg:'+key, username+':'+message);
-    });
+	return q(redisClient, 'incr', 'key:msg')
+		.then(function(key) {
+			let d = new Date();
+			let date = d.getDate()+'/'+ (parseInt(d.getMonth()) + 1) +'/'+d.getFullYear();
+			return q(redisClient, 'set', 'msg:'+key, username+':'+message+':'+date);
+		});
 }
 
 
 app = express();
 
 app.post('/', bodyParser.urlencoded(), function (req, res) {
-  if (!req.body.username || !req.body.message)
-    return res.redirect('/app/messages');
+	if (!req.body.username || !req.body.message)
+		return res.redirect('/app/messages');
 
-  add_message(req.body.username, req.body.message)
-    .then(function(messages) {
-      res.redirect('/app/messages');
-    })
-    .catch(function() {
-      res.render('error');
-    })
+	add_message(req.body.username, req.body.message) //!!
+		.then(function(messages) {
+			res.redirect('/app/messages');
+		})
+		.catch(function() {
+			res.render('error');
+		})
 });
 
 app.get('/', function (req, res) {
-  get_messages()
-    .then(function(messages) {
-      res.render('app/messages', { messages });
-    })
-    .catch(function() {
-      res.render('error');
-    })
+	get_messages()
+		.then(function(messages) {
+			res.render('app/messages', { messages });
+		})
+		.catch(function() {
+			res.render('error');
+		})
 });
 
 module.exports = app;
